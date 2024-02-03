11b201f3bf588f724a75fb273fc50323f709c7b7
Batozhab Budaev <pikapika1713@gmail.com>
2024-02-03 15:30:56 +0800

    *fix

--- src/screens/navigation/MainNavigation.tsx
+++ src/screens/navigation/MainNavigation.tsx
@@ -1,16 +1,80 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import MainScreen from '../MainScreen';
 
 const Tab = createBottomTabNavigator();
 
+const menuItems = [
+  {
+    id: '1',
+    name: 'Home',
+    icon: 'home',
+    label: 'Главная',
+    component: MainScreen,
+  },
+  {
+    id: '2',
+    name: 'Places',
+    icon: 'location',
+    label: 'Места',
+    component: MainScreen,
+  },
+  {
+    id: '3',
+    name: 'Impressions',
+    icon: 'map',
+    label: 'Впечатления',
+    component: MainScreen,
+  },
+  {
+    id: '4',
+    name: 'Housing',
+    icon: 'search',
+    label: 'Жилье',
+    component: MainScreen,
+  },
+  {
+    id: '5',
+    name: 'Profile',
+    icon: 'person',
+    label: 'Профиль',
+    component: MainScreen,
+  },
+];
+
 const MainNavigation = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name="Home" component={MainScreen} />
-        <Tab.Screen name="Settings" component={MainScreen} />
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarLabelStyle: {
+            fontFamily: 'SF-Pro-Display-Regular',
+            color: '#3D4252',
+            paddingBottom: 5,
+          },
+          tabBarStyle: {padding: 5, height: 60},
+        }}>
+        {menuItems.map(menuItem => (
+          <Tab.Screen
+            key={menuItem.id}
+            name={menuItem.name}
+            component={menuItem.component}
+            options={{
+              tabBarLabel: menuItem.label,
+              tabBarIcon: ({focused}) => (
+                <Ionicons
+                  name={focused ? menuItem.icon : `${menuItem.icon}-outline`}
+                  color="#3D4252"
+                  size={24}
+                />
+              ),
+            }}
+          />
+        ))}
       </Tab.Navigator>
     </NavigationContainer>
   );
